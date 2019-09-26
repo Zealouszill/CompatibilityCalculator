7e777f72d76075f315c4760e17031448f2c7e636
S. Stewart <rubix.this@gmail.com>
2019-09-26 16:44:14 -0600

    Added a bunch of stuff. Doesn't work still.

---
+++ CompatibilityCalculatorLogic/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CompatibilityCalculatorLogic
+{
+    public class MainViewModel
+    {
+    }
+}
---
+++ CompatibilityCalculatorTypes/IDataStorage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CompatibilityCalculatorTypes
+{
+    public class IDataStorage
+    {
+        void AddPotential(Potential p);
+        void AddUserStats(UserProfileStats u);
+        IEnumerable<Potential> GetAllPotentials();
+        Potential GetPotentialById(int id);
+        UserProfileStats GetUserStats();
+        void ChangeUserStats(UserProfileStats u);
+        string RemovePotentialById(int id);
+    }
+}
