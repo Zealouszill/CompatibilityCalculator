6fb67b8989de37afcfc6149d27d4adcf466c62df
S. Stewart <rubix.this@gmail.com>
2019-09-28 14:13:36 -0600

    Moved the project closer to the root. I guess path name is too long somewhere. Got it all to compile and run the UWP properly.

--- CompatibilityCalculatorXamarin/CompatibilityCalculatorXamarin.Android/MainActivity.cs
+++ CompatibilityCalculatorXamarin/CompatibilityCalculatorXamarin.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CompatibilityCalculatorXamarin.Droid
 {
-    [Activity(Label = "CompatibilityCalculatorXamarin", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HW11Xamarain", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -18,16 +18,8 @@ namespace CompatibilityCalculatorXamarin.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
