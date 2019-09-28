f49625cc7035abd55711793ce945bdd27c068a9f
S. Stewart <rubix.this@gmail.com>
2019-09-28 13:11:42 -0600

    Added a bunch of Nuget Packages. Still have errors. Creating save point.

---
+++ CompatibilityCalculator/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+
+namespace CompatibilityCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- CompatibilityCalculatorTypes/Potential.cs
+++ CompatibilityCalculatorTypes/Potential.cs
@@ -1,6 +1,6 @@
 ﻿
 
-namespace CompatibilityCalculator
+namespace CompatibilityCalculatorTypes
 {
     public class Potential
     {
